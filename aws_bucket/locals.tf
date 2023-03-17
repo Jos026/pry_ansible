2904174df7be02583a1d463865f3f2c274878967
Jos026 <josue.ocampo.soto@gmail.com>
2023-03-17 13:55:24 -0600

    testing runner

--- aws_bucket/locals.tf
+++ aws_bucket/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  vpc_id           = "vpc-0cffd490d24478589"
+  vpc_id           = "vpc-05dee8806cb3a5dce"
   ssh_user         = "ubuntu"
   key_name         = "devops"
   private_key_path = "/home/jos/.ssh/id_rsa"
-  ami_id           = "ami-0f0bad5ff61123617"
+  ami_id           = "ami-0574da719dca65348"
 }
