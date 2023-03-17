abe059a07596d590e20dd4aeaff62ff12e2ad799
Jos026 <josue.ocampo.soto@gmail.com>
2023-03-17 13:20:51 -0600

    test3

--- aws_bucket/locals.tf
+++ aws_bucket/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  vpc_id           = "vpc-05dee8806cb3a5dce"
+  vpc_id           = "vpc-0cffd490d24478589"
   ssh_user         = "ubuntu"
   key_name         = "devops"
   private_key_path = "/home/jos/.ssh/id_rsa"
